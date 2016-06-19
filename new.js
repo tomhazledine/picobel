fb8b090bcfdd0981b689a333dc7cce089b85bafc
Tom Hazledine <tom@tomhazledine.com>
2016-06-19 22:44:36 +0100

    pseudo-code for rebuild

---
+++ new.js
@@ -0,0 +1,33 @@
+// Find <audio> elements
+
+// Get file URL for each
+
+// Build custom markup
+//  * play/pause button
+//  * current time
+//  * total time
+//  * current time indicator (scrobbleable)
+//  * artist
+//  * track title
+//  * album title (?)
+
+// Load files into buffer
+
+// Loading indicator
+
+// Display file details
+
+// Get file metadata
+
+// Play specific file
+
+// Options:
+//  * theme (wrapper class)
+//  * playlist or independent players
+
+// Methods:
+//  * playFile(n) [n = file index]
+//  * pauseFile(n)
+//  * playFileFromTime(n,mmss) [mmss = minutes/seconds]
+//  * scrollToTime(n,mmss)
+//  * setVolume(d) [d = decimal volume level]
