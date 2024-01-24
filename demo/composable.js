589569c5f4c078b090845f1073112fe0e3ae6ef2
Tom Hazledine <tom@tomhazledine.com>
2024-01-24 17:22:38 +0000

    adding composable component options to demo page

--- demo/composable.js
+++ demo/composable.js
@@ -1,6 +1,8 @@
 import picobel from "../build/picobel.js";
 
 const section01 = document.querySelector("#section-01");
+const section02 = document.querySelector("#section-02");
+const section03 = document.querySelector("#section-03");
 
 const instance01 = picobel({
     theme: "bare",
@@ -8,4 +10,13 @@ const instance01 = picobel({
     context: section01
 });
 
-console.log({ instance01 });
+const instance02 = picobel({
+    theme: "bare",
+    // components: ["playPause", "progress"],
+    context: section02
+});
+const instance03 = picobel({
+    theme: "bare",
+    components: ["playPause", ["progress", ["duration", "timer"]]],
+    context: section03
+});
