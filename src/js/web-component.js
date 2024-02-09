ff36f24ab335cd281ff87fbeeb0f8f8672391e6c
Tom Hazledine <tom@tomhazledine.com>
2024-02-09 16:06:20 +0000

    web-components draft 01

---
+++ src/js/web-component.js
@@ -0,0 +1,24 @@
+import { picobel } from "./Picobel.js";
+
+// Create a class for the element
+class PicobelWC extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const className = this.classList[0] || "default";
+        const theme = this.getAttribute("data-theme") || className;
+        console.log({ theme });
+        const options = { theme, context: this };
+        const components = this.getAttribute("data-components");
+        if (components) {
+            options.components = JSON.parse(components);
+        }
+        picobel(options);
+    }
+}
+
+if (typeof window !== "undefined" && "customElements" in window) {
+    window.customElements.define("picobel-player", PicobelWC);
+}
---
+++ src/js/web-component.skeleton.js
@@ -0,0 +1,36 @@
+import { picobel } from "./Picobel.js";
+
+import styles from "../../build/picobel.skeleton.css";
+
+// Create a class for the element
+class PicobelWC extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    mountStyles(theme = "skeleton") {
+        const styleId = `picobel-styles-${theme}`;
+        if (!document.getElementById(styleId)) {
+            const styleEl = document.createElement("style");
+            styleEl.id = styleId;
+            // styleEl.textContent = styles[theme];
+            styleEl.textContent = styles;
+            document.head.appendChild(styleEl);
+        }
+    }
+
+    connectedCallback() {
+        const theme = this.getAttribute("data-theme") || "skeleton";
+        const options = { theme, context: this };
+        const components = this.getAttribute("data-components");
+        if (components) {
+            options.components = JSON.parse(components);
+        }
+        picobel(options);
+        this.mountStyles(theme);
+    }
+}
+
+if (typeof window !== "undefined" && "customElements" in window) {
+    window.customElements.define("picobel-player-skeleton", PicobelWC);
+}
